eea1824928a3c803bd186771458af42f96603cc8
BrenoAndrade03 <brenopereira263@gmail.com>
2022-02-08 11:14:01 -0300

    Estrutura de dados simples 1.0

---
+++ script.js
@@ -0,0 +1,16 @@
+var filmes =[ 'Encanto', 'Madagascar', 'Irmão urso', 'Procurando Nemo', 'Dragão Guerreiro']
+        console.log(filmes);
+    filmes.unshift('Rock dog')
+        console.log(filmes);
+    filmes.pop();
+        console.log(filmes)
+    filmes.push('Como treinar o seu dragão' , 'Avatar');
+        console.log(filmes);
+    filmes.shift();
+        console.log(filmes);
+
+//bonus
+const numbers= [7,5,6,3,8,9,2,1,4];
+    console.log(numbers);
+numbers.sort((a, b) => a-b);
+    console.log(numbers);
